c25edfb188fb33c80bbb6064cdbb75e144978a78
louisdevzz <vohuunhan1310@gmail.com>
2025-01-17 23:53:04 +0700

    fix: deploy in vercel

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    domains: ["hebbkx1anhila5yf.public.blob.vercel-storage.com"]
-  }
 };
 
 export default nextConfig;
